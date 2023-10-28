ab486b037414d1fea3bb9a1b67c0b2154553ecfe
Igor Costa <igorfelipe.costa@hotmail.com>
2023-10-28 17:16:26 -0300

    Adicionando email module

--- BackEnd/CompanyModule/DTOs/SecurityDTOs/LoginCNPJDTO.cs
+++ BackEnd/CompanyModule/DTOs/SecurityDTOs/LoginCNPJDTO.cs
@@ -2,13 +2,8 @@
 {
     public class LoginCNPJDTO
     { 
-<<<<<<< HEAD
-        public string Cnpj { get ; set; } = string.Empty;
-        public string Password { get ; set; } = string.Empty;
-=======
         public string Cnpj { get ; set; } 
         public string Password { get ; set; } 
->>>>>>> main
 
     }
 }
--- BackEnd/CompanyModule/DTOs/SecurityDTOs/LoginEmailDTO.cs
+++ BackEnd/CompanyModule/DTOs/SecurityDTOs/LoginEmailDTO.cs
@@ -2,12 +2,7 @@
 {
     public class LoginEmailDTO
     {
-<<<<<<< HEAD
-        public string Email { get; set; } = string.Empty;
-        public string Password { get; set; } = string.Empty;
-=======
         public string Email { get; set; }
         public string Password { get; set; }
->>>>>>> main
     }
 }
